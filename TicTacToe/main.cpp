198184d4665c948d6c98daaf590308e668ec8419
Szymon Wnuk <szwnuk1@gmail.com>
2024-07-31 23:01:11 +0200

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Szymon Wnuk on 31/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
